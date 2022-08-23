925c7a7f402ea1b433df640061ac3f63f423d0f4
luistorres17 <72280767+luistorres17@users.noreply.github.com>
2022-08-23 00:27:59 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,82 @@
+// Cuando el usuario hace scroll ejecuto myFuncion
+window.onscroll = function() {myFunction()};
+
+var secciones = document.getElementsByTagName("section");
+var fechas = document.getElementsByClassName("fecha");
+
+function myFunction(){
+  if (window.pageYOffset >= secciones[0].offsetTop) {
+    eliminarClase();
+  }
+  if (window.pageYOffset >= secciones[1].offsetTop - 100) {
+    eliminarClase();
+    fechas[0].classList.add("seleccionado");
+  }
+  if (window.pageYOffset >= secciones[2].offsetTop - 100) {
+    eliminarClase();
+    fechas[1].classList.add("seleccionado");
+  }
+  if (window.pageYOffset >= secciones[3].offsetTop - 100) {
+    eliminarClase();
+    fechas[2].classList.add("seleccionado");
+  }
+  if (window.pageYOffset >= secciones[4].offsetTop - 100) {
+    eliminarClase();
+    fechas[3].classList.add("seleccionado");
+  }
+  if (window.pageYOffset >= secciones[5].offsetTop - 100) {
+    eliminarClase();
+    fechas[4].classList.add("seleccionado");
+  }
+  if (window.pageYOffset >= secciones[6].offsetTop - 100) {
+    eliminarClase();
+    fechas[5].classList.add("seleccionado");
+  }
+  if (window.pageYOffset >= secciones[7].offsetTop -100) {
+    eliminarClase();
+    fechas[6].classList.add("seleccionado");
+  }
+  if (window.pageYOffset >= secciones[8].offsetTop -100) {
+    eliminarClase();
+    fechas[7].classList.add("seleccionado");
+  }
+  if (window.pageYOffset >= secciones[9].offsetTop -100) {
+    eliminarClase();
+    fechas[8].classList.add("seleccionado");
+  }
+  if (window.pageYOffset >= secciones[10].offsetTop -100) {
+    eliminarClase();
+    fechas[9].classList.add("seleccionado");
+  }
+  if (window.pageYOffset >= secciones[11].offsetTop -100) {
+    eliminarClase();
+    fechas[10].classList.add("seleccionado");
+  }
+  if (window.pageYOffset >= secciones[12].offsetTop -100) {
+    eliminarClase();
+    fechas[11].classList.add("seleccionado");
+  }
+  if (window.pageYOffset >= secciones[13].offsetTop -100) {
+    eliminarClase();
+    fechas[12].classList.add("seleccionado");
+  }
+  if (window.pageYOffset >= secciones[14].offsetTop -100) {
+    eliminarClase();
+    fechas[13].classList.add("seleccionado");
+  }
+  if (window.pageYOffset >= secciones[15].offsetTop -100) {
+    eliminarClase();
+    fechas[14].classList.add("seleccionado");
+  }
+  if (window.pageYOffset >= secciones[16].offsetTop -100) {
+    eliminarClase();
+    fechas[15].classList.add("seleccionado");
+  }
+  
+}
+
+function eliminarClase(){
+  for(i=0; i < fechas.length;i++){
+      fechas[i].classList.remove("seleccionado");
+    }
+}
